ac63d4ba95586dde9495cb34caaf26fb68e7bf69
Wertie <email@private.com>
2019-08-10 21:04:30 +0800

    upload files
    directory -- Modules

---
+++ Modules/Locals/Paths.py
@@ -0,0 +1,13 @@
+'''
+This module stores path constants.
+
+They are relative to the current working directory
+where toplevel scripts are.
+'''
+
+MATERIALS = r'..\Materials'
+SAVE = r'Save'
+
+GIF  = r'DCL.gif'
+ICON = r'pygame.ico'
+TEMP = r'TEMP'
