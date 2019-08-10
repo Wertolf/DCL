8b1756dd0a106cd6bf161df015c65154bd208ba0
Wertolf <32032140+Wertolf@users.noreply.github.com>
2019-08-10 19:17:44 +0800

    Upload files

---
+++ freezeLogin.bat
@@ -0,0 +1,8 @@
+pyinstaller ^
+--console ^
+--noconfirm ^
+--path="C:\Windows\System32\downlevel" ^
+--name Login ^
+--icon="pygame.ico" ^
+Login.py
+pause
---
+++ freezeStartGame.bat
@@ -0,0 +1,8 @@
+pyinstaller ^
+--console ^
+--noconfirm ^
+--path="C:\Windows\System32\downlevel" ^
+--name StartGame ^
+--icon="pygame.ico" ^
+StartGame.py
+pause
